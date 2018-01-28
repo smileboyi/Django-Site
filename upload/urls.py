397bfc910eca27a97482c2811945b270d6b46937
smileboyi <15215212143@163.com>
2018-01-28 22:01:40 +0800

    csv

--- upload/urls.py
+++ upload/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
   url(r'^register/$', views.registerNormalUser),
   url(r'^uploadFile/$', views.upload_file),
+  url(r'^csv/$', views.csv_view),
+  url(r'^csv2/$', views.csv_view2),
+  url(r'^pdf/$', views.pdf_view),
+  url(r'^pdf2/$', views.pdf_view2),
   
 ]
--- upload/views.py
+++ upload/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,render_to_response
+from django.template import loader, Context
 from django.http import HttpResponse
+from reportlab.pdfgen import canvas
 from django import forms
+from io import BytesIO
 from .models import *
+import reportlab
+import csv
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -67,3 +72,98 @@ def registerNormalUser(request):
     uf=NormalUserForm() # 实例化空表单
 
   return render(request,'upload/register.html',{'uf':uf})
+
+
+
+
+# 使用系统自带的csv
+def csv_view(request):
+  # Create the HttpResponse object with the appropriate CSV header.
+  response = HttpResponse(content_type='text/csv')
+  # 响应对象获取了附加的Content-Disposition协议头，它含有CSV文件的名称。文件名可以是任意的；你想把它叫做什么都可以。浏览器会在”另存为“对话框中使用它，或者其它。
+  # Content-Disposition以'attachment'开头，强制让浏览器弹出对话框来提示或者确认
+  response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+  # csv.writer 函数接受一个类似于文件的对象，而HttpResponse 对象正好合适。
+  # 如果数据量大，可以使用StreamingHttpResponse
+  writer = csv.writer(response)
+  writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+  writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+  return response
+
+# 使用Django的csv
+def csv_view2(request):
+  # Create the HttpResponse object with the appropriate CSV header.
+  response = HttpResponse(content_type='text/csv')
+  response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+  # The data is hard-coded here, but you could load it from a database or
+  # some other source.
+  csv_data = (
+      ('First row', 'Foo', 'Bar', 'Baz'),
+      ('Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"),
+  )
+  # 使用模板
+  t = loader.get_template('upload/my_template_name.txt')
+  c = Context({
+    'data': csv_data,
+  })
+  response.write(t.render(c))
+  return response
+
+
+
+def pdf_view(request):
+  # 创建带有PDF头部定义的HttpResponse对象
+  response = HttpResponse(content_type='application/pdf')
+  response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+  # 创建一个PDF对象，并使用响应对象作为它要处理的‘文件’
+  # Content-Disposition以'attachment'开头，强制让浏览器弹出对话框来提示或者确认
+  p = canvas.Canvas(response)
+
+  # 通过PDF对象的drawString方法，写入一条信息。具体参考模块的官方文档说明。
+  p.drawString(100, 100, "Hello world.")
+
+  # 关闭PDF对象
+  p.showPage()
+  p.save()
+  return response
+
+
+def pdf_view2(request):
+  # Create the HttpResponse object with the appropriate PDF headers.
+  response = HttpResponse(content_type='application/pdf')
+  response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+  # 使用ReportLab创建复杂的PDF文档时，可以考虑使用io库作为PDF文件的临时保存地点。
+  buffer = BytesIO()
+
+  # Create the PDF object, using the BytesIO object as its "file."
+  p = canvas.Canvas(buffer)
+
+  # Draw things on the PDF. Here's where the PDF generation happens.
+  # See the ReportLab documentation for the full list of functionality.
+  p.drawString(100, 100, "Hello world2.")
+
+  # Close the PDF object cleanly.
+  p.showPage()
+  p.save()
+
+  # Get the value of the BytesIO buffer and write it to the response.
+  pdf = buffer.getvalue()
+  buffer.close()
+  response.write(pdf)
+  return response
+
+
+
+
+
+
+
+
+
+
+
+
