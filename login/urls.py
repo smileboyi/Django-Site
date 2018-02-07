60e3583cc2a061ef5d922e30ba5f532000c1c88c
smileboyi <15215212143@163.com>
2018-02-07 18:01:54 +0800

    login

--- login/urls.py
+++ login/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'login'
 urlpatterns = [
-	url(r'^index/', views.index),
-	url(r'^login/', views.login),
-	url(r'^register/', views.register),
-	url(r'^logout/', views.logout),
+	url(r'^index/$', views.index),
+	url(r'^login/$', views.login),
+	url(r'^register/$', views.register),
+	url(r'^logout/$', views.logout),
 ]
--- login/views.py
+++ login/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from . import models
 # Create your views here.
 
 def index(request):
-  if request.method == "POST":
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    print(username, password)
-    return redirect('/index/')
+  
   return render(request, 'login/index.html')
 
+
 def login(request):
-  return render(request, 'login/login.html')
+  if request.method == "POST":
+    # 获取数据
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    message = "所有字段都必须填写！"
+    # 验证用户
+    if username and password:
+      username = username.strip()
+      # 用户是否存在
+      try:
+        user = models.User.objects.get(name=username)
+        # 用户密码是否正确
+        if user.password == password:
+          return redirect('login/index/')
+        else:
+          message = "密码不正确！"
+      except:
+        message = "用户名不存在！"
+    # 登录失败，附上错误提示信息
+    return render(request, 'login/login.html', {"message": message})
+  # 成功登录
+  return render(request, 'login/index.html')
+
 
 def register(request):
   return render(request, 'login/register.html')
 
+
 def logout(request):
-  return redirect("/index/")
+  return redirect("login/index/")
